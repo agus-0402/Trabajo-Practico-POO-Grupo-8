a77f8ccbf506bd323cfcdc47cd6e077f51de3a5a
Javier12301 <javierramirez1230123@gmail.com>
2022-06-07 09:40:20 -0300

    Se cambio de lado al boton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(255, 238);
+            this.button1.Location = new System.Drawing.Point(440, 110);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
